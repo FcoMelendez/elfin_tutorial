7452fef222e5701e040f62b2c41f6fede250eddc
FcoMelendez <fco.melendez.f@gmail.com>
2023-10-11 15:42:55 +0200

    Update .eslintrc.js

--- elfin_adapter/.eslintrc.js
+++ elfin_adapter/.eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-  'extends': "tamia",
-  'parserOptions': {
-    'sourceType': "module"
-  },
-  'plugins': ["prettier"],
-  'rules': {
-    "prettier/prettier": "error",
-    "valid-jsdoc": 0
-  }
+    extends: "tamia",
+    parserOptions: {
+        sourceType: "module"
+    },
+    plugins: ["prettier"],
+    rules: {
+        "prettier/prettier": "error",
+        "valid-jsdoc": 0
+    }
 };
